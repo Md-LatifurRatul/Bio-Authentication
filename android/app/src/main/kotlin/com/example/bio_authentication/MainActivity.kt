4852d02326cb529490738735b1dd848e0104c598
Md-LatifurRatul <latifursoikot@gmail.com>
2025-10-15 03:18:04 +0600

    add biometric, local authentication

--- android/app/src/main/kotlin/com/example/bio_authentication/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/bio_authentication/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.example.bio_authentication
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterFragmentActivity()
--- lib/presentation/screen/bio_authentication_home_screen.dart
+++ lib/presentation/screen/bio_authentication_home_screen.dart
@@ -1,10 +1,84 @@
 import 'package:flutter/material.dart';
+import 'package:local_auth/local_auth.dart';
 
-class BioAuthenticationHomeScreen extends StatelessWidget {
+class BioAuthenticationHomeScreen extends StatefulWidget {
   const BioAuthenticationHomeScreen({super.key});
 
   @override
+  State<BioAuthenticationHomeScreen> createState() =>
+      _BioAuthenticationHomeScreenState();
+}
+
+class _BioAuthenticationHomeScreenState
+    extends State<BioAuthenticationHomeScreen> {
+  bool _isAuthenticated = false;
+  final LocalAuthentication _auth = LocalAuthentication();
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(body: _buildUI(), floatingActionButton: _authButton());
+  }
+
+  Widget _authButton() {
+    return FloatingActionButton(
+      onPressed: () async {
+        if (_isAuthenticated) {
+          final bool canAuthenticateWithBiometric =
+              await _auth.canCheckBiometrics;
+          print(canAuthenticateWithBiometric);
+          if (canAuthenticateWithBiometric) {
+            try {
+              final bool didAuthenticate = await _auth.authenticate(
+                localizedReason: "Please Authenticate first",
+
+                options: const AuthenticationOptions(biometricOnly: true),
+              );
+
+              setState(() {
+                _isAuthenticated = didAuthenticate;
+              });
+            } catch (e) {
+              print(e.toString());
+            }
+          }
+        } else {
+          setState(() {
+            _isAuthenticated = false;
+          });
+        }
+
+        setState(() {
+          _isAuthenticated = !_isAuthenticated;
+        });
+      },
+      child: Icon(_isAuthenticated ? Icons.lock : Icons.lock_open),
+    );
+  }
+
+  Widget _buildUI() {
+    return SizedBox(
+      width: MediaQuery.sizeOf(context).width,
+
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Text(
+            "Account Balance",
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          ),
+          if (_isAuthenticated)
+            const Text(
+              "\$ 25,23232",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+          if (!_isAuthenticated)
+            const Text(
+              "******",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+        ],
+      ),
+    );
   }
 }
